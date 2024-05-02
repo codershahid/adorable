47f76f20bceac56d2a9c6168c5c03733de2f74e7
codershahid <codershahid@icloud.com>
2024-05-02 09:29:31 +0530

    implemented code-spliting with lazy loading componentz

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
-import Details from "./Details";
 import AdoptedPetContext from "./AdoptedPetContext";
+const Details = lazy(() => import("./Details"));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   const adoptedPetHook = useState(null);
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: Infinity,
-        cacheTime: Infinity,
-      },
-    },
-  });
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPetHook}>
-          <Link to={"/"}>
-            <h2>Adopt Me!</h2>
-          </Link>
-          <p>Find your perfect furry, feathered, or scaly buddy through adoption at Adorable!</p>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader"> 🐶 </h2>
+              </div>
+            }
+          >
+            <Link to={"/"}>
+              <h2>Adopt Me!</h2>
+            </Link>
+            <p>Find your perfect furry, feathered, or scaly buddy through adoption at Adorable!</p>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
         </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
